1ace526595efc4409a1618715a9d18edc21301af
Nataliia Volkova <volkov.nataliia@gmail.com>
2026-01-20 12:50:31 +0000

    Revert "websocket logic"
    This reverts commit cd5e3b97089931cb949cba0ea0938e63b7771500.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import cors from "cors";
-import http from "http";
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-export let messages = [
+let messages = [
   {
     message_id: 1,
     username: "Sara",
@@ -31,7 +30,7 @@ app.get("/messages", (req, resp) => {
     return resp.status(400).json({ error: "Invalid since format" });
   }
   const newMessages = messages.filter(
-    (msg) => !sinceId || msg.message_id > sinceId,
+    (msg) => !sinceId || msg.message_id > sinceId
   );
   if (newMessages.length > 0) {
     resp.json(newMessages);
@@ -60,9 +59,6 @@ app.post("/messages", (req, resp) => {
   resp.status(200).json(newMessage);
 });
 
-const server = http.createServer(app);
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server listen on ${port}`);
 });
-
-export default server;
